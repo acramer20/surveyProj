351270910da1b82cf4c5b14dc1a1d463e5fee324
acramer20 <97561089+acramer20@users.noreply.github.com>
2022-03-04 12:28:55 -1000

    adding sessions to app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from crypt import methods
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 from random import randint, choice, sample
 from surveys import Question, Survey, surveys, satisfaction_survey
+
+"""Must define RESPONSES_KEY for sessions"""
+RESPONSES_KEY = 'responses'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "dont-tell!"
@@ -10,21 +13,24 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route('/')
 def title_page():
-    
+    """Home page to start survey"""
     return render_template('title_page.html', survey=surveys)
 
 
 @app.route('/start', methods=['POST'])
 def question_0():
+    """using sessions to create a list to be passed back and forth bewteen browser and server
+    reditecting to the first question"""
+    session[RESPONSES_KEY] = []
     return redirect('/questions/0')
 
 @app.route('/questions/<int:quid>')
 def show_question(quid):
-
+    """dynamically rendering the question based on the quid that we get from the /answer route"""
+    responses = session.get(RESPONSES_KEY)
 
     if (len(responses) != quid):
         # Trying to access questions out of order.
@@ -41,8 +47,17 @@ def show_question(quid):
 
 @app.route('/answer', methods=["POST"])
 def save_answer():
-    """saving answer to list and redirecting to next question"""
-    responses.append(request.form['answer'])
+    """saving answer to list and redirecting to next question by comparing to how many answers are in the list"""
+    
+    choice = request.form['answer']
+    responses = session[RESPONSES_KEY]
+    responses.append(f"{choice}")
+    session[RESPONSES_KEY] = responses
+
+    # print("*******************")
+    # print(session.get(RESPONSES_KEY))
+    # print("**************")
+
     quid = len(responses)
 
     if len(responses) == 4:
@@ -52,7 +67,7 @@ def save_answer():
 
 @app.route('/complete')
 def complete_survey():
-    """Just letting them know they are done with survey"""
+    """Just letting them know they are done with survey when the list length is equal to the num of questions"""
     return render_template('complete.html')
 
 
